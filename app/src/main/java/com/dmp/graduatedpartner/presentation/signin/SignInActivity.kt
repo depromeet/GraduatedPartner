cb2ffaaee3f8400bff047d9d155bfa8b204765f8
pete <pete.jeong@kakaomobility.com>
2019-04-15 13:08:03 +0900

    [HYEONSEOK-2] SignIn 레이아웃 일부 작업

--- app/src/main/java/com/dmp/graduatedpartner/presentation/signin/SignInActivity.kt
+++ app/src/main/java/com/dmp/graduatedpartner/presentation/signin/SignInActivity.kt
@@ -1,12 +1,15 @@
 package com.dmp.graduatedpartner.presentation.signin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.ViewPager
 import com.dmp.graduatedpartner.R
 import com.dmp.graduatedpartner.databinding.ActivitySigninBinding
 import com.dmp.graduatedpartner.presentation.base.BaseActivity
 import com.dmp.graduatedpartner.presentation.signin.anim.FadePageTransformer
+import com.dmp.graduatedpartner.presentation.start.StartActivity
 import kotlinx.android.synthetic.main.activity_signin.*
 import org.koin.android.viewmodel.ext.android.viewModel
 
@@ -23,12 +26,16 @@ class SignInActivity : BaseActivity() {
         initViewPager()
     }
 
-    private fun initViewPager(){
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun initViewPager() {
         updateUI(0)
         viewpager_signin.offscreenPageLimit = 3
         viewpager_signin.setPageTransformer(false, FadePageTransformer())
         viewpager_signin.adapter = SignInViewPagerAdapter()
-        viewpager_signin.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener(){
+        viewpager_signin.addOnPageChangeListener(object : ViewPager.SimpleOnPageChangeListener() {
             override fun onPageSelected(position: Int) {
                 updateUI(position)
                 super.onPageSelected(position)
@@ -36,7 +43,15 @@ class SignInActivity : BaseActivity() {
         })
     }
 
-    private fun updateUI(position: Int){
+    private fun updateUI(position: Int) {
 
+    }
+
+    fun onPressBackButton(view: View) {
+        val nextIntent = Intent(
+            this@SignInActivity,
+            StartActivity::class.java
+        ).addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(nextIntent)
     }
 }
