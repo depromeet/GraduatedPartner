770e69b2588352a0e24797768405e9bdc22f8b6a
jjy <skwntnftkgk@gmail.com>
2019-04-27 01:06:37 +0900

    졸업요건 complete 추가

--- app/src/main/java/com/dmp/graduatedpartner/model/GradRequ.kt
+++ app/src/main/java/com/dmp/graduatedpartner/model/GradRequ.kt
@@ -1,3 +1,3 @@
 package com.dmp.graduatedpartner.model
 
-data class GradRequ(val type: Int?, val title: String?)
+data class GradRequ(val type: Int?, val title: String?, val isComplete: Boolean = false)
